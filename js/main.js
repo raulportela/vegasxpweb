4eff806da55290196317798c633716b19150f178
raulportela <raul.p.p@hotmail.com>
2021-07-27 19:58:09 -0300

    Atualização da tela principal, faltando somente mecher na tag main.

--- js/main.js
+++ js/main.js
@@ -1,5 +1,11 @@
-function onClickMenu(){
-	document.getElementById("menu").classList.toggle("change");
-	document.getElementById("nav").classList.toggle("change");
-	document.getElementById("menu-bg").classList.toggle("change-bg")
-}
+const menuBtn = document.querySelector('.menu-btn');
+let menuOpen = false;
+menuBtn.addEventListener('click', () => {
+  if(!menuOpen) {
+    menuBtn.classList.add('open');
+    menuOpen = true;
+  } else {
+    menuBtn.classList.remove('open');
+    menuOpen = false;
+  }
+});
